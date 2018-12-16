bb90a06508ef2e75e2a7562ff99d8616f651f41e
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-16 11:26:18 -0500

    Sun Dec 16 11:26:18 EST 2018

--- plugin_load.go
+++ plugin_load.go
@@ -80,7 +80,7 @@ func PluginAlert(measure []byte) (string, string, bool, error) {
 	alertMsg  := ""
 	alertLvl  := ""
 	alertFlag := false
-	alertErr  := errors.New("")
+	alertErr  := errors.New("no error")
 
 	// Check that the CPU overall value is within range
 	switch {
